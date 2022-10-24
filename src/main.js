0c2482f8903b81163af4bbfda0cae03b52378d00
Boofk <fajzov.amir.gl@gmail.com>
2022-10-24 22:30:14 +0500

    #1 - Установил зависимости, подключил все в main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './routes/router.js'
+import { createPinia } from 'pinia'
+
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import './style.css'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app
+    .use(ElementPlus)
+    .use(pinia)
+    .use(router)
+    .mount('#app')
