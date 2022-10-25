0030ce0078f66ed8e44dd3f5db4fd189991654f1
Boofk <fajzov.amir.gl@gmail.com>
2022-10-25 18:17:06 +0500

    #6 - Подправил данные для отправки в body в stt

--- src/services/api/speech-to-text.js
+++ src/services/api/speech-to-text.js
@@ -1,14 +1,12 @@
 import HttpClient from '../http-client.js'
 import {speechToText} from '../config.js'
 
-const convert = async ({ogg}) => {
+const convert = async (formData) => {
     const httpClient = new HttpClient(speechToText.URL, speechToText.TOKEN)
 
     return await httpClient.makeRequest({
         url: '/stt/',
-        data: {
-            ogg
-        }
+        data: formData
     })
 }
 
