e2bbfcc98cb283f383bd75710ae2822c519ea210
am1rfy <am>
2022-10-26 23:24:50 +0500

    Фикс бага при переходе на главную страницу

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
-        name: 'stt',
+        name: 'Home',
         meta: {
             title: 'Speech To Text'
         },
