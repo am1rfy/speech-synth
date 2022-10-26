93cabd7ec1097715ebf2575bc09cc22383230d87
Boofk <fajzov.amir.gl@gmail.com>
2022-10-26 22:25:19 +0500

    #13 - Сделал 404 страницу

--- src/views/not-found-page.vue
+++ src/views/not-found-page.vue
@@ -1,13 +1,47 @@
 <template>
+  <div class="main-container">
+    <el-empty
+        id="empty"
+        image-size="250"
+        description="This Page Not Found"
+    />
+    <div class="link-container">
+      <el-link
+          type="success"
+          @click="$router.push({name: 'tts'})"
+      >
+        Text To Speech
+      </el-link>
 
+      <el-link
+          type="success"
+          @click="$router.push({name: 'stt'})"
+      >
+        Speech To Text
+      </el-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "not-found-page"
+  name: 'notFoundPage'
 }
 </script>
 
 <style scoped>
-
+  body {
+    display: flex;
+    justify-content: center;
+  }
+  .main-container {
+    margin-top: 10vh;
+  }
+  .link-container {
+    display: flex;
+    justify-content: space-between;
+  }
+  #empty {
+    cursor: default;
+  }
 </style>
